42154369d2d9de2312999153444f693f29d32074
biswasjishnu25 <jishnubiswas3@gmail.com>
2023-08-22 16:04:54 +0530

    setting defalut workspace

---
+++ buffer.py
@@ -0,0 +1,4 @@
+import arcpy
+
+# setting the default workspace
+arcpy.env.workspace = r"D:\3rd_semester\Programming_for_GIS_III\P1_python_script\Practical_1_ProProject\01_Running_Python_Scripts.gdb"
