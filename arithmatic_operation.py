161401f908c2be47b63acc38ddd5b1665e21dfb2
biswasjishnu25 <jishnubiswas3@gmail.com>
2023-08-29 14:01:52 +0530

    Arithmatic operations

---
+++ arithmatic_operation.py
@@ -0,0 +1,16 @@
+num1 = 10
+num2 = 25
+addition = num1 + num2
+print("the additoin of {} and {} is {}".format(num1, num2, addition))
+
+subtraction = num1 - num2
+print("the subtraction of {} and {} is {}".format(num1, num2, subtraction))
+
+multiplication = num1 * num2
+print("the multiplication of {} and {} is {}".format(num1, num2, multiplication))
+
+division = num1 / num2
+print("the division of {} and {} is {}".format(num1, num2, division))
+
+modulus = num1 % num2
+print("the reminder of {} and {} is {}".format(num1, num2, modulus))
